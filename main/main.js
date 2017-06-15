010c54ce3d1a3502ce4b97229e3823a95861dd05
ashui <ashui@ashuihouse.cn>
2017-06-15 21:00:19 +0800

    1
    1

--- main/main.js
+++ main/main.js
@@ -1,3 +1,9 @@
 module.exports = function main(email, suffixes) {
   // Write your code here
+  var var1=new Array();
+  var1=email.split("@");
+  if(var1[1]==suffixes)
+    console.log(`true`);
+  else
+    console.log(`false`);
 };
